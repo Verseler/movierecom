c02af0409dd413ec5e3427827616b385588ff873
verseler <handuman.verselerkerr@gmail.com>
2024-10-15 19:33:05 +0800

    feat: add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
 const env = loadEnv(mode, process.cwd(), "");
@@ -13,6 +14,41 @@ return {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "movierecom",
+        short_name: "movierecom",
+        theme_color: "#171717",
+        icons: [
+          {
+            src: "appIcon.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icon-512-maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 }
 })
