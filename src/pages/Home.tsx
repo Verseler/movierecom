49765fcc9422b7dcd1bdca1c0719d3c723228b8a
Verseler <vrslr2003@gmail.com>
2025-07-18 22:16:43 +0800

    feat: add trending movie catalog

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import FilmHeroSection from "@/features/film/FilmHeroSection";
 import FilmCarousel from "@/features/film/FilmCarousel";
-import { useGetFilmsQuery } from "@/features/film/filmApi";
+import {
+  useGetFilmsQuery,
+  useGetTrendingFilmsQuery,
+} from "@/features/film/filmApi";
 import { getFilmId } from "@/features/film/film.helper";
 import AlignContainer from "@/components/AlignContainer/AlignContainer";
+import FilmCatalog from "@/features/film/FilmCatalog";
+import { useState } from "react";
 
 export default function Home() {
   const { data: popularMovies, isLoading: popularMoviesIsLoading } =
@@ -12,19 +17,30 @@ export default function Home() {
 
   const featuredMovieId: number = getFilmId(popularMovies?.results[0]);
 
+  const [page, setPage] = useState(1);
+  const { data: trendingMovies, isLoading: trendingMoviesIsLoading } =
+    useGetTrendingFilmsQuery({ filmType: "movie", page: page });
+  const loadMorePage = () => setPage(page + 1);
+
   return (
     <div className="overflow-y-hidden">
       <FilmHeroSection id={featuredMovieId} />
       <AlignContainer className="py-10 space-y-10">
         <FilmCarousel
+          label="Popular Series"
+          data={popularSeries?.results}
+          isLoading={popularSeriesIsLoading}
+        />
+        <FilmCarousel
           label="Popular Movies"
           data={popularMovies?.results}
           isLoading={popularMoviesIsLoading}
         />
-        <FilmCarousel
-          label="Popular Series"
-          data={popularSeries?.results}
-          isLoading={popularSeriesIsLoading}
+        <FilmCatalog
+          label="Trending Movies"
+          data={trendingMovies?.results}
+          isLoading={trendingMoviesIsLoading}
+          loadMorePage={loadMorePage}
         />
       </AlignContainer>
     </div>
